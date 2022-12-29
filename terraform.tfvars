d4826aae8943eec828a8dafa40e5e8213f61aa6f
John Fabry <jjfabry@pm.me>
2022-12-29 10:04:38 -0500

    Adds tfvars and removes defaults from variables.tf.

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+instance_name = "Terraform Cloud Managed VM"
+instance_size = "t2.micro"
+instance_ami = "ami-0283a57753b18025b"
--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "Terraform Cloud Managted VM"
 }
 
 variable "instance_size" {
   description = "VM size in AWS."
   type        = string
-  default     = "t2.micro"
 }
 
 variable "instance_ami" {
   description = "AMI for the image to deploy."
   type        = string
-  default     = "ami-0283a57753b18025b"
 }
 
